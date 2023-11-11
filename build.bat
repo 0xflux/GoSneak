2c69cfb45c31c7d2a87cc5096fb1db8784ccddc2
Ian G <49762827+the-wandering-photon@users.noreply.github.com>
2023-11-11 10:53:17 +0000

    build assembly via ml64

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 g++ -c .\c_dll\test_dll.cpp -o .\c_dll\test_dll.o
 x86_64-w64-mingw32-gcc -shared .\c_dll\test_dll.o -o .\c_dll\outputfile.dll
+ml64 /c /Zi .\syscalls.asm
 g++ -c -o inj.o inj.cpp -DUNICODE
-g++ inj.o -o inj.exe
+g++ inj.o syscalls.obj -o inj.exe
